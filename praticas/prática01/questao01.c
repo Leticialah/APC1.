23a0081781caa17beb1a4259b2ecff63617984a9
Leticialah <142408340+Leticialah@users.noreply.github.com>
2023-09-18 13:49:35 +0000

    18/09

--- praticas/prática01/questao01.c
+++ praticas/prática01/questao01.c
@@ -5,12 +5,20 @@ Faça um programa em C que calcule a média final a partir da fórmula (0,4 x A1
 #include <stdio.h>
 
 int main() {
-  float nota_a1 = 9.5f;
-  float nota_a2 = 8.0f;
+  char materia[61];
+  float valor_a1;
+  float valor_a2;
+
+  printf("Entre com a nota da A1");
+  scanf("%s", materia);
+  printf("Entre com o valor do item 1: ");
+  scanf("%f", valor_a1);
+  printf("Entre com o valor do item 1: ");
+  scanf("%f", valor_a2);
 
   float media = 0.4 * nota_a1 + 0.6 * nota_a2;
 
-  printf("A média é %f\n", media);
+  printf("Media %f\n", media);
 
   return 0;
   
