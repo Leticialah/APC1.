13d8f0b09fb0199b2dcd7927c2cfcc34ab2c09a4
Leticialah <142408340+Leticialah@users.noreply.github.com>
2023-11-06 12:12:05 +0000

    06/11

--- aulas/aula05/menu.c
+++ aulas/aula05/menu.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
   char opcao = 0;
@@ -8,34 +8,73 @@ int main() {
   while(1) {
     int ok = system("clear");
     printf("MENU PRINCIPAL\n");
-    printf("1 - Consultar saldo\n");
-    printf("2 - Fazer recarga\n");
-    printf("3 - Listar recados\n");
-    printf("0 - Sair\n");
+    printf("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n");
+    printf("█  1 - Consultar saldo                                     █\n");
+    printf("█  2 - Fazer recarga                                       █\n");
+    printf("█  3 - Listar recados                                      █\n"); 
+    printf("█  0 - Sair                                                █\n"); 
+    printf("█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n");
     printf("Sua escolha => ");
     ok = scanf("%c", &opcao);
     getchar();
 
     switch(opcao) {
       case '1': 
+        ok = system("clear");
+        printf("CONSULTA SALDO\n");
         printf("Seu saldo é %.2f\n", saldo); 
+        printf("Pressione ENTER para continuar...");
+        getchar();
         break;
-      case '2': 
-        printf("Escolha um valor de recarga: \n"); break;
+      case '2': {
+        char valor = 0;
+        while (valor != '0') {
+          ok = system("clear");
+          printf("FAZER RECARGA\n");
+          printf("1 - R$ 10,00\n");
+          printf("2 - R$ 20,00\n");
+          printf("5 - R$ 50,00\n");
+          printf("Escolha um valor de recarga ou digite 0 para retornar: ");
+          ok = scanf("%c", &valor);
+          getchar();
+
+          switch(valor) {
+            case '1': saldo = saldo + 10.0f; break;
+            case '2': saldo = saldo + 20.0f; break;
+            case '5': saldo = saldo + 50.0f; break;
+            case '0': break;
+            default: 
+              printf("Opção inválida!\n"); 
+              printf("Pressione ENTER para continuar...");
+              getchar();
+              break;
+          }
+
+          if (valor == '1' || valor == '2' || valor == '5') {
+            printf("\e[0;32mRecarga realizada com sucesso!\e[0m\n");
+            printf("Pressione ENTER para continuar...");
+            getchar();
+          }
+        }    
+        break;
+      }
       case '3': 
-        printf ("Você não tem recados\n"); 
+        ok = system("clear");
+        printf("LISTAR RECADOS\n"); 
+        printf("Você não tem recados\n"); 
+        printf("Pressione ENTER para continuar...");
+        getchar();
         break;
       case '0': break;
-      default: printf("Opção inválida!\n"); 
+      default: 
+        printf("\e[0;31mOpção inválida!\e[0m\n");
+        printf("Pressione ENTER para continuar...");
+        getchar();
         break;
-      
     }
 
     if (opcao == '0') break;
-
-    printf("Pressione ENTER para continuar...");
-    getchar();
   }
-  
+
   return 0;
 }
